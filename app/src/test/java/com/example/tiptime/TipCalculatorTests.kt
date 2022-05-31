b5beb3c991020195164f36cf8d5dcc6f9e4d098c
Andrea Bonatti <andreabonatti92@gmail.com>
2022-05-31 11:19:06 +0200

    4) Automated Tests

---
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,17 @@
+package com.example.tiptime
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+// Local Test -> On the computer
+class TipCalculatorTests {
+    @Test
+    fun calculate_20_percentage_tip_no_roundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+//        val expectedTip = "$2.00"
+        val expectedTip = "2,00 €"
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
